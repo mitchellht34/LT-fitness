5f9ecbfd42c182900d99d2ebf974cd90ea9b95a1
mitchellht34 <mitchellht34@gmail.com>
2024-12-12 21:25:05 -0500

    make workout display workouts for individual user only

--- client/js/workouts-page.js
+++ client/js/workouts-page.js
@@ -3,10 +3,10 @@ retrieveData();  //Get inital load
 function retrieveData() {
     // Retrieve the library data and populate on page load
     $.ajax({
-        url: exerciseURL + "/read-records",
+        url: exerciseURL + "/read-workouts",
         type: "get",
         success: function(response){
-            console.log(response);
+            console.log("response: " + response);
             var data = JSON.parse(response);
             createExerciseTable(data.exercises);
         },
@@ -20,7 +20,19 @@ function createExerciseTable(exerciseData) {
     console.log(exerciseData);
 
     var tableHTML = "";
+    var day = exerciseData[0].workout_plan_id;
+    tableHTML += "<tr>";
+    console.log(day);
+    console.log(exerciseData[0].workout_plan_id)
+    tableHTML += "<td>Day " + ((day % 3)) + "</td>";
     for(var i=0; i<exerciseData.length; i++) {
+        if(exerciseData[i].workout_plan_id != day){
+            tableHTML += "<tr>";
+            console.log(day);
+            console.log(exerciseData[i].workout_plan_id)
+            tableHTML += "<td>Day " + ((day % 3) + 1) + "</td>";
+            day++;
+        }
         tableHTML += "<tr>";
         tableHTML += "<td>" + exerciseData[i].exercise_name + "</td>";
         // tableHTML += "<td>" + exerciseData[i].bookTitle + "</td>";
--- server/services.js
+++ server/services.js
@@ -11,6 +11,8 @@ const connection = mysql.createConnection({
     if (err) throw err;
     console.log('Connected to MySQL!');
   });
+
+let currentUser;
   
 
 var services_sql = function(app) {
@@ -26,6 +28,8 @@ var services_sql = function(app) {
             if (results.length > 0) {
                 req.session.loggedin = true;
                 req.session.username = username;
+                currentUser = results[0].user_id;
+                console.log(currentUser);
                 res.redirect('/home');
             } else {
                 res.send('Incorrect Username and/or Password!');
@@ -43,12 +47,15 @@ app.post('/sign-up', function(req, res) {
     // console.log(res);
     let username = req.body.username;
     let password = req.body.password;
+    currentUser= req.body.user_id;
     if (username && password) {
         connection.query('SELECT * FROM users WHERE username = ?', [username], function(err, results, fields) {
             if (err) throw err;
             if (results.length != 0 && results[0].password == password) {
               req.session.loggedin = true;
               req.session.username = username;
+              currentUser = results[0].user_id;
+              console.log(currentUser);
               res.redirect('/home');
             } else if (results != 0 && results[0].password != password){
               res.send("User already exists") 
@@ -62,6 +69,11 @@ app.post('/sign-up', function(req, res) {
                 if (err) throw (err)
                 req.session.loggedin = true;
                 req.session.username = username;
+                connection.query('SELECT user_id FROM users WHERE username = ?', [username], function(err, results, fields) {
+                    if (err) throw (err)
+                    currentUser = results[0].user_id;
+                    console.log(currentUser);
+                })
                 res.redirect('/home');
               })            }			
       });
@@ -141,8 +153,19 @@ app.get('/home', function(req, res) {
           
     });
 
-    app.get('/read-records', function(req, res) {
-          connection.query("SELECT * FROM exercises", function(err, rows) {
+    app.get('/read-workouts', function(req, res) {
+          console.log("user " + currentUser);
+          connection.query(`
+            SELECT workout_plan.workout_plan_id, exercises.exercise_name
+            FROM users
+	        JOIN workout_plan
+		        ON workout_plan.routine = users.routine
+            JOIN exercise_plan_workouts
+		        ON workout_plan.workout_plan_id = exercise_plan_workouts.workout_plan_id
+	        JOIN exercises
+		        ON exercises.exercise_id = exercise_plan_workouts.exercise_id
+            WHERE user_id = ?`, currentUser
+            , function(err, rows) {
               if(err) {
                   throw err;
               } else {
@@ -152,6 +175,19 @@ app.get('/home', function(req, res) {
               }
           });    
     });
+
+    /*app.get('/read-records', function(req, res) {
+        connection.query("SELECT * FROM exercises", function(err, rows) {
+            if(err) {
+                throw err;
+            } else {
+              console.log("Read Records");
+              // connection.end();
+              return res.status(201).send(JSON.stringify({msg:"SUCCESS", exercises:rows}));
+            }
+        });    
+  });*/
+
 
 }
 
